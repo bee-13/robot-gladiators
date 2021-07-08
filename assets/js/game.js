102735d9cb8f04f8e0928d1cc518007731639ab5
Biggy <bilalersoy740@gmail.com>
2021-07-08 18:33:30 -0400

    aranged code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,25 +1,42 @@
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-// What is this?
-console.log(playerName);
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logs a string, good for leaving yourself a message");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// this will do math and log 20
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+  
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  
+    // Log a resulting message to the console so we know that it worked.
+  
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  
+    // Log a resulting message to the console so we know that it worked.
 
-console.log(10 + 10);
+    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 
-// what is this?
+    enemyHealth = enemyHealth - playerAttack;
 
-console.log("Our robot's name is " + playerName);
+    // Log a resulting message to the console so we know that it worked.
 
-var name = "your name";
-console.log(name);
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        );
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
+    playerHealth = playerHealth - enemyAttack;
 
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        );
+  };
 
-// this creates a function named "fight"
-
-function fight() {
-    window.alert("The fight has begun!");
-  }
-  //fight();
+fight();
